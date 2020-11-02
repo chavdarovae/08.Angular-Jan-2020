279d7d6c2f3ffceb9e5edd6905b3c6cc16ff6fb3
chavdarovae <chavdarova.e@gmail.com>
2020-11-03 00:20:31 +0100

    Initial commit of the first two themes

---
+++ 01. INTRO TO ANGULAR AND TYPESCRIPT/ts-demo/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(arg1, arg2) {
+    return arg1 + arg2;
+}
+var a = sum(234, 345);
+console.log(a);
---
+++ 01. INTRO TO ANGULAR AND TYPESCRIPT/ts-demo/src/index.ts
@@ -0,0 +1,7 @@
+function sum (arg1:number, arg2:number) {
+    return arg1+arg2;
+}
+
+const a = sum (234, 345);
+
+console.log(a);
